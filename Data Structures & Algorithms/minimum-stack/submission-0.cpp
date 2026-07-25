79efea6e0269c20e68d3a5641dcfd6218a10b017
Akshay Gupta <85879402+akshaygupta1407@users.noreply.github.com>
2026-07-25 10:44:12 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.cpp
@@ -0,0 +1,38 @@
+class MinStack {
+public:
+    stack<int>st1, st2;
+    MinStack() {
+        
+    }
+    
+    void push(int value) {
+        st1.push(value);
+        if(st2.empty() || value <= getMin()) {
+            st2.push(value);
+        }
+    }
+    
+    void pop() {
+        if(st1.top() == getMin()) {
+            st2.pop();
+        }
+        st1.pop();
+    }
+    
+    int top() {
+        return st1.top();
+    }
+    
+    int getMin() {
+        return st2.top();
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(value);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
